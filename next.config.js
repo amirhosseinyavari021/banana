5285a54a890859c9647a069ae5850705e5d6f12a
AY021 <amirhosseinyavari61@gmail.com>
2025-02-16 22:22:54 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'export',
-  basePath: '/banana',
+  basePath: '/banana', 
   images: {
     unoptimized: true
   }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig; 
