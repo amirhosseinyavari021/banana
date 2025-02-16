1d8806a141d2b8445cd0059e402bdfd739ef7840
AY021 <amirhosseinyavari61@gmail.com>
2025-02-16 13:44:56 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  reactStrictMode: true, // حالت Strict برای دیباگ بهتر
+  trailingSlash: true, // اضافه کردن / به انتهای URL
+  output: "export", // تولید خروجی استاتیک
+  images: {
+    unoptimized: true, // حل مشکل تصاویر در GitHub Pages
+  },
+  basePath: "/banana", // نام ریپو گیت‌هاب رو اینجا بذار
+  assetPrefix: "/banana", // برای فایل‌های استاتیک
 };
+
 module.exports = nextConfig;
